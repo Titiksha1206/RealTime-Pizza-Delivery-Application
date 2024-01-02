9a08d6127c951cb82bc45504deb153dafcfb0396
titiksha <titiksha1206@gmail.com>
2024-01-03 04:28:45 +0530

    second commit

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const ejs = require("ejs");
 const expressEjsLayouts = require("express-ejs-layouts");
+
+// inbuilt module h .
 const path = require("path");
 
 const app = express();
@@ -8,6 +10,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
+  //  home.ejs file to keep html.
   res.render("home");
 });
 
@@ -15,7 +18,14 @@ app.listen(port, () => {
   console.log(`server is running at Port: ${port}`);
 });
 
-// set Template engine
+// assets
+app.use(express.static("public"));
+
+// express ko pata chlega ki konsa layout yaha pr use krna h.
 app.use(expressEjsLayouts);
+
+// express ko hme batana h ki jo hmari views folder (contains html file) vo kaha pr h .
 app.set("views", path.join(__dirname, "/resources/views"));
+
+// set Template engine (express ko batana h ki hmm konsa template engine use krne wale h ).
 app.set("view engine", "ejs");
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,3 +1,7 @@
 let mix = require("laravel-mix");
 
-mix.js("src/app.js", "dist").setPublicPath("dist");
+// first parameter means konsi file aapko compile krni h.
+// second parameter means kaha pr aapko usko store krna h, compile hone ke baad.
+mix
+  .js("resources/js/app.js", "public/js/app.js")
+  .sass("resources/scss/app.scss", "public/css/app.css");

269267023d5626f9e47dbe0d6a0c5df2c9a49eca
titiksha <titiksha1206@gmail.com>
2024-01-19 20:58:52 +0530

    register crud

--- src/http/controllers/authController.js
+++ src/http/controllers/authController.js
@@ -1,4 +1,6 @@
 const User = require("../../models/userModels");
+const bcrypt = require("bcrypt");
+
 function authController() {
   return {
     login(req, res) {
@@ -9,16 +11,52 @@ function authController() {
       res.render("auth/register");
     },
 
-    postRegister(req, res) {
+    async postRegister(req, res) {
       // jo bhi data hmm send krne wale h vo hmme millega req.body se.
       const { name, email, password } = req.body; //name, email, password hmee millenge req.body se.
 
       // validate request.
       if (!name || !email || !password) {
         req.flash("error", "All fields are required.");
+
+        // error anne pr dala hua data erase nhi hona chahiye.
+        req.flash("name", name);
+        req.flash("email", email);
+
         return res.redirect("/register");
       }
-      res.render("auth/postRegister");
+
+      // email should be unique at each registration.
+      // check if email exists.
+      const userr = await User.exists({ email: email });
+      //agr already database ke andr email exist krta hai toh error showkro.
+      if (userr) {
+        req.flash("error", "Email already taken");
+        req.flash("name", name);
+        req.flash("email", email);
+        return res.redirect("/register");
+      }
+
+      // hash password.
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      // create a user.
+      const user = new User({
+        name,
+        email,
+        password: hashedPassword,
+      });
+
+      user
+        .save()
+        .then((user) => {
+          // login
+          return res.redirect("/");
+        })
+        .catch((err) => {
+          req.flash("error", "Something went wrong.");
+          return res.redirect("/register");
+        });
     },
   };
 }
